7b92ce2d57a5a5bfd94d54c471bf67444c112758
Penny20160 <ajg.sikma@gmail.com>
2026-09-05 13:22:18 +0200

    Update blogdata.js

--- blogdata.js
+++ blogdata.js
@@ -1,25 +1,32 @@
 const data = [{
     "label": "week 1",
     "dagen": [{
-        "datum":"maandag 31-08-2026"
+        "datum":"maandag 31-08-2026",
         "entries": [{
             "text":"Dit zou mijn eerste dag op stage zijn geworden, maar er ging wat mis. René was te laat en kon mijn nummer niet vinden. Ik heb wel zelf meerdere malen naar het nummer van Art Salvage geprobeerd te bellen, maar er werdt niet opgenomen. Uiteindelijk ben ik op aanraden van mijn docent naar huis gegaan. Ik heb toen ik thuis was gebeld en toen de afspraak voor de volgende dag gemaakt."
-        }]},{
-        "datum":"dinsdag 01-09-2026"
+        }]
+    },
+              {
+        "datum":"dinsdag 01-09-2026",
             "entries":[{
                 "text":"Vandaag was mijn werkelijke eerste dag. Ik moest om half negen op het kantoorpand van Art Salvage zijn. Ik was er op tijd en werd door René binnen gelaten. Nadat Renate en René op waren gestart werd mij de uitleg gegeven van wat zij van mij verwachten. In de periode tussen mijn aanneming en nu hebben ze twee grote klussen gekregen. Één voor de kunstobjecten van de gemeente Enschede en één voor de kunstobjecten van de gemeente Vlaardingen. Wat dit voor mij betekent is dat ik de inventarisatie en inspectieformulieren van die objecten mag invoeren. Dit is iets anders dan de calamiteitenplannen die er eerst op de planning stonden maar dat is niet erg. Ik ben gelijk begonnen met het invullen van die inspectieformulieren van de gemeente Enschede in hun computer sinds ik met mijn computer niet op hun server kan. Ik heb bij het eerste formulier nog wel even om feedback gevraagd om zeker te weten dat ik het op de juiste manier deed. Ongeveer halverwege de dag werd er een afspraak gemaakt om een kunstobject op te halen. Dit was voor de volgende dag. Ik mag hier mee naar toe. Ik heb uiteindelijk vier formulieren ingevuld, ook omdat ik de adressen er bij moet zoeken, en de gemeente Enschede geen huisnummers bij de straat van hun kunstobjecten zet. Leuk als je object moet vinden op een straat van 5 kilometer. Daarnaast heeft de gemeente Enschede niet alle objecten opgeslagen dus ... ." 
-            }]},{
-        "datum":"woensdag 02-09-2026"
+            }]
+    },
+              {
+        "datum":"woensdag 02-09-2026",
             "entries":[{
                 "text":"Vandaag mocht ik mee met het ophalen van een kunstobject. Het object moest alleen eerst restaureer klaar gemaakt worden. Dit betekende dat er eerst drie lagen met baksteen van de gedenksteen afgebikt moesten worden. Ik mocht meehelpen met bikken en moest fotograve spelen. Stenen bikken is best wel zwaar. Ik begrijp nu waar ze op doelden toen ze in de grijze jager veertien zeiden dat steenhouwers stenen bikken het ergste vonden wat er was. Ik heb mezelf aan een blaar op m'n duim geholpen door mee te werken. Toen ik de blaar had heb ik de bezem ter hande gepakt zodat ik nog wel wat deed. Zodat het werk is het overigens nog misgegaan met de steen. In eerste instantie brak er een stuk steen af terwijl we aan het bikken waren en bij de laatste aantal stenen brak de gedenksteen doormidden. Ik heb overigens niet gezien wanneer precies want op dat punt moest ik een accu van een van de machines halen. De foto's die hieronder staan zijn overigens van tijdens het werk en van voor de breuk. Ik heb daarna nog wel foto's gemaakt met hun camera, maar ben vergeten ze te maken met mijn mobiel. Toen we klaar waren hebben we de laatste steenresten in de bak die daarvoor was klaargezet gegooid. Daarna zijn we naar huis gegaan."
-            }]},{
-        "datum":"donderdag 03-09-2026"
+            }]
+              },
+              {
+        "datum":"donderdag 03-09-2026",
          "entries":[{
              "text":"Ik heb vandaag maar een halve dag gedraaid, omdat ik op donderdag ambulante begeleiding heb. Ik had dit al wel voordat ik begon besproken met René. Vandaag was wel weer een computerdag wat betekende dat ik weer verder ging met de inspectieformulieren invullen. Daarnaast heb ik de foto's van de inventariesatie deels genummerd volgens de nummering van Art Salvage. Dit zodat ik wat minder hoef te zoeken als ik de foto's erbij moet pakken. Vandaag had ik wat minder problemen met het opzoeken van de adressen van de objecten. Ik heb 3 objecten geïnventariseerd vandaag."
          }]
              
-    },{
-        "datum":"vrijdag 04-09-2026"
+    },
+        {
+        "datum":"vrijdag 04-09-2026",
          "entries":[{
              "text":"Laatste dag van de eerste week. Vandaag heb ik 6,5 inventarisatie formulier ingevuld. Ik heb vandaag ook besloten in google Earth een kaart te maken van de kunstobjecten van de gemeente Enschede. Ik had vandaag overigens weer een probleem gehad met het vinden van een object. Deze zou ergens op het terrein van de technische universiteit van Twente staan. Na zo'n vier keer de hele campus te zijn rond geweest heb ik het maar opgegeven. Ik heb dit wel even gemeld. Verderrest ging het best goed vandaag. Aan het einde van de dag heb ik samen met degene die in het atelier werkt het atelier schoongemaakt. Sorry dat ik je naam ben vergeten persoon. Ik heb het best prima naar mijn zin gehad deze eerste week."
          }]
