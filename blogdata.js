73d193cb83b69e5314ae078eb1bc2489818f9671
Penny20160 <ajg.sikma@gmail.com>
2026-09-05 11:05:35 +0200

    Update blogdata.js

--- blogdata.js
+++ blogdata.js
@@ -1,8 +1,12 @@
 const data = [{
     "label": "week 1",
     "dagen": [{
+        "datum":"maandag 31-08-2026"
         "entries": [{
-            "text":"de stage is nog niet begonnen."
-        }]
-    }]}]
+            "text":"Dit zou mijn eerste dag op stage zijn geworden, maar er ging wat mis. René was te laat en kon mijn nummer niet vinden. Ik heb wel zelf meerdere malen naar het nummer van Art Salvage geprobeerd te bellen, maar er werdt niet opgenomen. Uiteindelijk ben ik op aanraden van mijn docent naar huis gegaan. Ik heb toen ik thuis was gebeld en toen de afspraak voor de volgende dag gemaakt."
+        }]},{
+        "datum":"dinsdag 01-09-2026"
+            "entries":[{
+                "text":"Vandaag was mijn werkelijke eerste dag. Ik moest om half negen op het kantoorpand van Art Salvage zijn. Ik was er op tijd en werd door René binnen gelaten. Nadat Renate en René op waren gestart werd mij de uitleg gegeven van wat zij van mij verwachten. In de periode tussen mijn aanneming en nu hebben ze twee grote klussen gekregen. Één voor de kunstobjecten van de gemeente Enschede en één voor de kunstobjecten van de gemeente Vlaardingen. Wat dit voor mij betekent is dat ik de inventarisatie en inspectieformulieren van die objecten mag invoeren. Dit is iets anders dan de calamiteitenplannen die er eerst op de planning stonden maar dat is niet erg. Ik ben gelijk begonnen met het invullen van die inspectieformulieren van de gemeente Enschede in hun computer sinds ik met mijn computer niet op hun server kan. Ik heb bij het eerste formulier nog wel even om feedback gevraagd om zeker te weten dat ik het op de juiste manier deed. Ongeveer halverwege de dag werd er een afspraak gemaakt om een kunstobject op te halen. Dit was voor de volgende dag. Ik mag hier mee naar toe. Ik heb uiteindelijk vier formulieren ingevuld, ook omdat ik de adressen er bij moet zoeken, en de gemeente Enschede geen huisnummers bij de straat van hun kunstobjecten zet. Leuk als je object moet vinden op een straat van 5 kilometer. Daarnaast heeft de gemeente Enschede niet alle objecten opgeslagen dus ... ." 
+   ]}]
 
